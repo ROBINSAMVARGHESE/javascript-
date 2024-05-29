401441b62e071ae5c65ffa462b117938fb75cfa5
robinsamvarghese <robinsamvarghese990@gmail.com>
2024-05-29 11:57:53 +0530

    new commit made

---
+++ script.js
@@ -0,0 +1,18 @@
+let age =0-12;
+switch ( age) {
+    case 0-12:
+        console.log('child');
+        break;
+    case 13-19:
+        console.log('Teenger');
+        break;
+    case 20-59:
+        console.log('adult');
+        break;
+    case 60:
+        console.log('senior');
+        break;
+
+    default:
+        console.log('invalid age');
+ }
